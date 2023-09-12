37fdf501d37c9916028c598446a7a2ec014e9ca0
rahul-rautwar <rahulrautwar@gmail.com>
2023-09-12 21:36:52 +0530

    add main and secondary container

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,6 +1,8 @@
 import React from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   // custom hook to fetch now playing movies
@@ -9,6 +11,10 @@ const Browse = () => {
   return (
     <div>
       <Header />
+
+      {/* add main container and secondry container here */}
+      <MainContainer />
+      <SecondaryContainer />
     </div>
   );
 };
