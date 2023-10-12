f8efcafb078ed92362d1ca7e7d717e75cd96745f
rahul-rautwar <rahulrautwar@gmail.com>
2023-10-12 21:39:10 +0530

    add movie list component in secondary container

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import MovieList from "./MovieList";
 
 const SecondaryContainer = () => {
-  return <div>SecondaryContainer</div>;
+  const movies = useSelector((store) => store.movies);
+  return (
+    <div className="-mt-50 relative z-20">
+      <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+      <MovieList title={"Trending"} movies={movies?.nowPlayingMovies} />
+      <MovieList title={"Popular"} movies={movies?.nowPlayingMovies} />
+      <MovieList title={"Upcomings"} movies={movies?.nowPlayingMovies} />
+    </div>
+  );
 };
 
 export default SecondaryContainer;
