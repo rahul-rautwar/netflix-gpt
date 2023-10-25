5e67a7f1190bf1e2951920b6bc016ff128c2f2e3
rahul-rautwar <rahulrautwar@gmail.com>
2023-10-25 22:23:17 +0530

    update css for movie list

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -5,12 +5,16 @@ import MovieList from "./MovieList";
 const SecondaryContainer = () => {
   const movies = useSelector((store) => store.movies);
   return (
-    <div className="-mt-50 relative z-20">
-      <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
-      <MovieList title={"Trending"} movies={movies?.nowPlayingMovies} />
-      <MovieList title={"Popular"} movies={movies?.nowPlayingMovies} />
-      <MovieList title={"Upcomings"} movies={movies?.nowPlayingMovies} />
-    </div>
+    movies?.nowPlayingMovies && (
+      <div className="bg-black">
+        <div className="relative -mt-50 pl-12 z-20">
+          <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Trending"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Popular"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Upcomings"} movies={movies?.nowPlayingMovies} />
+        </div>
+      </div>
+    )
   );
 };
 
