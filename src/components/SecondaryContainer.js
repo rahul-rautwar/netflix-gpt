c1027e738ca3bc5225472b45f2d5c646af1aced8
rahul-rautwar <rahulrautwar@gmail.com>
2023-11-22 22:46:40 +0530

    update secondary container

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -9,9 +9,9 @@ const SecondaryContainer = () => {
       <div className="bg-black">
         <div className="relative -mt-50 pl-12 z-20">
           <MovieList title={"Now Playing"} movies={movies?.nowPlayingMovies} />
-          <MovieList title={"Trending"} movies={movies?.nowPlayingMovies} />
-          <MovieList title={"Popular"} movies={movies?.nowPlayingMovies} />
-          <MovieList title={"Upcomings"} movies={movies?.nowPlayingMovies} />
+          <MovieList title={"Top Rated"} movies={movies?.topRatedMovies} />
+          <MovieList title={"Popular"} movies={movies?.popularMovies} />
+          <MovieList title={"Upcomings"} movies={movies?.upcomingMovies} />
         </div>
       </div>
     )
